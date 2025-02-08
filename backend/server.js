fc65ff18a68e4404a9f9506b3d66e24631e2bb89
Faishal shaikh <faishalshaikh625@gmail.com>
2025-02-09 01:50:14 +0530

    complete project

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
 import router from './routes/product.route.js';
 
-dotenv.config();                         
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
@@ -11,11 +13,25 @@ const port = process.env.PORT;
 // Middleware
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
+
 // Routes
 app.use('/api/products', router);
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    connectDB();
+// Connect to the database and start the server
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch(err => {
+    console.error('Failed to connect to the database', err);
 });
